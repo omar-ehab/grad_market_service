bd7da0905428e35576b3e1dcf6fe4a54707a2424
afnan <afnan_samy@icloud.com.com>
2021-04-13 22:30:48 +0200

    market service after updates

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const { sequelize } = require('./models');
 const cors = require('cors');
+const axios = require('axios');
 require('dotenv').config();
 
 
 const market_routes = require("./routes/market");
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.SERVICE_PORT || 80;
 
 const app = express();
+
+const registerService = () => axios.put(`${process.env.APIGATEWAY_PROTOCOL}://${process.env.APIGATEWAY_HOST}:${process.env.APIGATEWAY_PORT}/register/${process.env.SERVICE_NAME}/${process.env.SERVICE_VERSION}/${PORT}`);
+
+
+
 
 
 app.use(helmet());
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json())
 
 
 //market routes
@@ -42,8 +49,18 @@ app.use((err, req, res) => {
 
 // CONNECTING TO HTTP SERVER .........
 
+// app.listen(PORT,async ()  => {
+//   console.log("\n [+] HTTP server is listening on port " + PORT + "\n");
+//   await sequelize.sync({force: true})
+// });
+
+//database listen
 app.listen(PORT, () => {
-  console.log("\n [+] HTTP server is listening on port " + PORT + "\n");
+
+  registerService();
+  setInterval(registerService, 25000);
+
+  console.log(`server running at http://127.0.0.1:${PORT}`);
 });
 
 
